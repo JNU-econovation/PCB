68597901d311600bb345cd4369420fa3f067eb9e
bada308 <203190@jnu.ac.kr>
2023-07-19 11:15:51 +0900

    Feat: Layout 추가 - Layout ui component - Home Page Layout 구현 - 이외 페이지 Layout 구현

---
+++ frontend/src/components/atoms/Layout.jsx
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components';
+
+const Layout = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+export default Layout;
